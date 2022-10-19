bc50d87e32e7aa0d80ca00210af00bdee4a1cd6b
Andre Gama del Otero <andre.gama@avalith.net>
2022-10-19 12:26:10 -0300

    button component

--- templates/nextjs-styled-components/components/atoms/Button/index.tsx
+++ templates/nextjs-styled-components/components/atoms/Button/index.tsx
@@ -1,29 +1,53 @@
 import styled from 'styled-components';
 
+const sizes = {
+  small: '1rem',
+  medium: '1.5rem',
+  large: '2rem',
+};
+
 const StyledButton = styled.button<{
   bgColor: string;
   color: string;
-  size: string;
+  size: 'small' | 'medium' | 'large';
+  upperCase?: boolean;
 }>`
-  background-color: bgColor;
-  color: color;
-  padding-inline: ${props => (props.size === 'medium' ? '2rem' : '1rem')};
+  background-color: ${props => props.bgColor};
+  color: ${props => props.color};
+  padding-inline: ${props => sizes[props.size]};
+  text-transform: ${props => (props.upperCase ? 'uppercase' : 'none')};
+  padding-block: 1rem;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  font-size: 1.2rem;
+  &:hover {
+    filter: saturate(70%);
+  }
 `;
 export type ButtonProps = {
   label: string;
   bgColor: string;
   color: string;
-  size: string;
+  size: 'small' | 'medium' | 'large';
+  upperCase?: boolean;
 };
 export const Button = ({
   label,
   bgColor,
   color,
-  size = 'medium',
+  upperCase,
+  size,
   ...props
 }: ButtonProps) => {
   return (
-    <StyledButton bgColor={bgColor} color={color} size={size} {...props}>
+    <StyledButton
+      bgColor={bgColor}
+      color={color}
+      size={size}
+      upperCase={upperCase}
+      {...props}
+    >
       {label}
     </StyledButton>
   );
