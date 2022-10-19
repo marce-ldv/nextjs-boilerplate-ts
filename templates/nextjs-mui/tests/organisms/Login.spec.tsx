1a63beb9cc20fd1a48c7441dd9bbb8f37e70a53e
marcelo-ludovino <marcelo.docutec@gmail.com>
2022-10-19 16:56:13 -0300

    add login tests

---
+++ templates/nextjs-mui/tests/organisms/Login.spec.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import LoginOrganism from '../../components/organisms/Login'
+
+describe('Organism Login', () => {
+  test('render LoginOrganism without crash', () => {
+    render(<LoginOrganism />)
+  })
+
+  test('render username label in LoginOrganism', () => {
+    render(<LoginOrganism />)
+    const label = screen.getByText('username')
+    expect(label).toBeInTheDocument()
+  })
+})
