dfd84443bcbcfe61d9e209f879eb1b8adb5fa7aa
Andre Gama del Otero <andre.gama@avalith.net>
2022-10-19 14:41:47 -0300

    added button story

--- templates/nextjs-styled-components/stories/atoms/Button.stories.tsx
+++ templates/nextjs-styled-components/stories/atoms/Button.stories.tsx
@@ -1,41 +1,45 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
-
+import { Button } from '../../components/atoms/Button';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  label: 'Primary',
+  bgColor: '#0000ff',
+  color: '#ffffff',
+  size: 'large',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Error = Template.bind({});
+Error.args = {
+  label: 'Error',
+  bgColor: '#ff0000',
+  color: '#ffffff',
+  size: 'medium',
 };
 
 export const Large = Template.bind({});
 Large.args = {
+  bgColor: '#ffe600',
+  color: '#000000',
   size: 'large',
-  label: 'Button',
+  label: 'large',
 };
 
 export const Small = Template.bind({});
 Small.args = {
+  bgColor: '#ffe600',
+  color: '#000000',
   size: 'small',
-  label: 'Button',
+  label: 'small',
 };
